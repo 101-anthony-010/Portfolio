b826ece5705b5591f44a055b5274f5df7de3ad52
antony <gonzalescamposantony@gmail.com>
2023-05-15 14:37:00 -0500

    error menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,11 +5,11 @@ import { changeIsShowMenu } from './Store/slices/menu.slice'
 const Menu = () => {
     const dispatch = useDispatch()
     const { isShowMenu } = useSelector(store => store.menuSlice)
-
+    console.log(isShowMenu)
     const handleClickChangeClose = () => dispatch(changeIsShowMenu())
   return (
-    <main className={`grid justify-end items-start fixed ${isShowMenu ? "right-0":"-right-full"} transition-all duration-700 w-full top-0 h-screen right-0 z-20`}>
-        <div onClick={handleClickChangeClose} className={`${isShowMenu ? "right-0":"-right-full"} transition-all duration-500 fixed top-0 right-0 w-full h-full bg-gray-500/20`}></div>
+    <main className={`grid justify-end items-start fixed ${isShowMenu ? "right-0":"-right-[100%]"} transition-all duration-700 w-full top-0 h-screen right-0 z-20`}>
+        <div onClick={handleClickChangeClose} className={`${isShowMenu ? "right-0" : "-right-[100%]"} transition-all duration-500 fixed top-0 right-0 w-full h-full bg-gray-500/20`}></div>
         <div className={`w-[500px] h-full bg-white relative rounded-md`}>
             <i onClick={handleClickChangeClose} className='top-0 right-0 absolute cursor-pointer p-2 text-xl hover:text-red-500 font-bold bx bx-x'></i>
             <div className='text-center pt-6'>
